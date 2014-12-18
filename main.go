dcc703974c29563e0515915d09ca5541e1c03645
Johannes 'fish' Ziemke <github@freigeist.org>
2014-12-18 10:13:26 +0100

    Add Basic Auth support

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
@@ -13,7 +14,26 @@ var (
 	metricsEndpoint  = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
 	addr             = flag.String("addr", "unix:///var/run/docker.sock", "Docker address to connect to")
 	parent           = flag.String("parent", "/docker", "Parent cgroup")
+	authUser         = flag.String("auth.user", "", "Username for basic auth.")
+	authPass         = flag.String("auth.pass", "", "Password for basic auth. Enables basic auth if set.")
 )
+
+type basicAuthHandler struct {
+	handler  http.HandlerFunc
+	user     string
+	password string
+}
+
+func (h *basicAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	user, password, ok := r.BasicAuth()
+	if !ok || password != h.password || user != h.user {
+		w.Header().Set("WWW-Authenticate", "Basic realm=\"metrics\"")
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		return
+	}
+	h.handler(w, r)
+	return
+}
 
 func main() {
 	flag.Parse()
@@ -23,6 +43,17 @@ func main() {
 	prometheus.MustRegister(exporter)
 
 	log.Printf("Starting Server: %s", *listeningAddress)
-	http.Handle(*metricsEndpoint, prometheus.Handler())
+	handler := prometheus.Handler()
+	if *authUser != "" || *authPass != "" {
+		if *authUser == "" || *authPass == "" {
+			glog.Fatal("You need to specify -auth.user and -auth.pass to enable basi  c auth")
+		}
+		handler = &basicAuthHandler{
+			handler:  prometheus.Handler().ServeHTTP,
+			user:     *authUser,
+			password: *authPass,
+		}
+	}
+	http.Handle(*metricsEndpoint, handler)
 	log.Fatal(http.ListenAndServe(*listeningAddress, nil))
 }
